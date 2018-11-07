4a23249e24f31f841a95c5d53f9ee7b2db72158e
markhuang <markhuang286@gmail.com>
2018-11-06 19:06:28 -0500

    Updates

---
+++ precache-manifest.e709f7c85f7aa1b24b60e440fdad6782.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robots/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "49d69044dcc19f0c7c57",
+    "url": "/robots/static/js/runtime~main.49d69044.js"
+  },
+  {
+    "revision": "ff091d9d5cdd7b846b94",
+    "url": "/robots/static/js/main.ff091d9d.chunk.js"
+  },
+  {
+    "revision": "e2e731c2e0b6e695703d",
+    "url": "/robots/static/js/1.e2e731c2.chunk.js"
+  },
+  {
+    "revision": "ff091d9d5cdd7b846b94",
+    "url": "/robots/static/css/main.1c6023f8.chunk.css"
+  },
+  {
+    "revision": "e2e731c2e0b6e695703d",
+    "url": "/robots/static/css/1.dd23733e.chunk.css"
+  },
+  {
+    "revision": "0b2e61b313316d0782277f09f3c561ab",
+    "url": "/robots/index.html"
+  }
+];
